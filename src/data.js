cf45016ab2cc73effd76f796372fb176585a31d8
Aphenphos <MaxwillWinters@gmail.com>
2022-11-22 22:31:28 -0500

    grammar change

--- src/data.js
+++ src/data.js
@@ -19,7 +19,7 @@ function makeAbout(title, content) {
 
 makeAbout('Thoughtful design makes life easier', 'With a background in psychology and management, well-planned and executed systems have always been my goal.')
 makeAbout('Endless learning', 'Nothing improves on its own, one must be willing to learn new and rapidly advancing technologies as time dictates.')
-makeAbout('Hard Work', ' No amount of talent is a replacement for dedication to putting in the hours required to maximize potential.')
+makeAbout('Hard Work', ' No amount of talent is a replacement for dedication and putting in the hours required to maximize potential.')
 
 
 makeProject("YouCal", "https://github.com/Aphenphos/calendar", 'Calendar app built entirely with React and Vanilla JS. Make a Calendar, add events and share your calendar with other users!');
