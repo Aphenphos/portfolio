f719f7cd48503cea270dd6a64a91daaa6299f65f
Aphenphos <MaxwillWinters@gmail.com>
2022-11-23 11:07:40 -0500

    fixed artifact on slide in effect

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -3,7 +3,7 @@ export default function Project({ name, githubLink, description, index }) {
   return (
     <>
       <div className="project-container" style={{
-        animation: `${(index / 2) + .75}s ease-out 0s 1 slideInLeft` }}>
+        animation: `${(index / 2) + .75}s ease-in 0s 1 slideInLeft` }}>
           <h1>{name}</h1>
           <p>{description}</p>
           <a href={githubLink}>Github</a>
