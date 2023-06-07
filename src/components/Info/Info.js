17765e8cd52b52f4895be2e76f81aa90f8ccb38e
Aphenphos <MaxwillWinters@gmail.com>
2023-06-07 19:06:52 -0400

    general styling updates and updated resume

--- src/components/Info/Info.js
+++ src/components/Info/Info.js
@@ -1,4 +1,3 @@
-
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import { faG } from "@fortawesome/free-solid-svg-icons";
@@ -8,17 +7,17 @@ library.add(faGithub, faLinkedin, faG);
 export default function Info() {
   return (
     <>
-    <div id='info-links'>
-      <a href="https://www.linkedin.com/in/maxwill-winters/">
-      <FontAwesomeIcon icon="fa-brands fa-linkedin" />
-      </a>
-      <a href='https://github.com/Aphenphos'>
-        <FontAwesomeIcon icon="fa-brands fa-github" />
-      </a>
-      <a href='mailto: MaxwillWinters@gmail.com'>
-        <FontAwesomeIcon icon="fa-g" />
-      </a>
+      <div id="info-links">
+        <a href="https://www.linkedin.com/in/maxwill-winters/">
+          <FontAwesomeIcon icon="fa-brands fa-linkedin" />
+        </a>
+        <a href="https://github.com/Aphenphos">
+          <FontAwesomeIcon icon="fa-brands fa-github" />
+        </a>
+        <a href="mailto: MaxwillWinters@gmail.com">
+          <FontAwesomeIcon icon="fa-g" />
+        </a>
       </div>
-      </>
+    </>
   );
 }
